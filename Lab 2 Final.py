9bc69ede8f0d0bf6918cc01670c3005ec463aae2
Arhiiiip <90906424+Arhiiiip@users.noreply.github.com>
2021-12-12 13:20:27 +0300

    Add files via upload

--- Lab 2 Final.py
+++ Lab 2 Final.py
@@ -1,97 +1,111 @@
-import csv, time, requests
+import csv
+import requests
 from bs4 import BeautifulSoup
-#Функция для фильтрации параметров имеющих числовое значение:
-def num_val(obj, min_val):
+
+
+def sort_num_val(obj, min_val):
     if obj == 'Unknown':
         obj = 0
     if float(obj) >= min_val:
         return True
     else:
         return False
-#Функция для фильтрации параметров, которые не обязательно должны встрчаться все разом:
-def lists(obj, spiski):
-    for i in spiski:
-        if i in obj:
+
+
+def sort_list(obj, list_user):
+    if list_user == '':
+        return True
+    for j in list_user:
+        if j in obj:
             return True
         else:
             return False
-#Отдельная функция для фильтрации по жанру, так как для поиска можно использовать несколько жанров и они все должны учитываться:
-def genre(obj, tags_user):
-    for i in tags_user:
-        if i not in obj:
-            return False
-        else:
-            return True
-#Функция, для проверки на соответсвие с запросом:
-def filter_(param, condF, comp, obj):
-    if condF(obj[param], comp):
+
+
+def sort_(param, conditional, comp, obj):
+    if conditional(obj[param], comp):
         return True
     else:
         return False
 
-print('Какой минимальный рейтинг вас интересует? Введите число, если оно не целое то используйте точку для разделения целой и десятичной части. Если это для вас не важно нажмите Enter.')
-min_rating = input()
-try:
-    min_rating = float(min_rating)
-except:
-    if min_rating == '':
-        print('')
-    else:
-        print('К сожелению вы ввели не число или вообще ничего не ввели, значит это не будет учтено(')
-        time.sleep(0.5)
-    min_rating = 0
-print('Какое минимальное количество голосов вас интересует? Если это для вас не важно нажмите Enter.')
-min_number_votes = input()
-try:
-    min_number_votes = float(min_number_votes)
-except:
-    if min_number_votes == '':
-        print('')
-    else:
-        print('К сожелению вы ввели не число или вообще ничего не ввели, значит это не будет учтено(')
-        time.sleep(0.5)
-    min_number_votes = 0
 
-print('Какие жанры вас интересуют? Если это для вас не важно нажмите Enter.')
-tags_user = input().split(", ")
-print('Какие предупреждения вас интересуют? Если это для вас не важно нажмите Enter.')
-con_war = input().split(", ")
-print('Какие типы вас интерсует? Если это для вас не важно нажмите Enter.')
-type_user = input().split(", ")
-print('Какое минимальное количество эпизодов вас интересует? Если это для вас не важно нажмите Enter.')
-min_episodes = input()
-try:
-    min_episodes = float(min_episodes)
-except:
-    if min_episodes == '':
-        print('')
-    else:
-        print('К сожелению вы ввели не число или вообще ничего не ввели, значит это не будет учтено(')
-        time.sleep(0.5)
-    min_episodes = 0
-print('Важно для вас чтобы аниме ещё продолжало выпускатьcя или уже закончило? Введите "True" или "False". Если это для вас не важно нажмите Enter.')
-finish_user = input()
-print('Какие годы начала съёмки аниме вас интересует? Если это для вас не важно нажмите Enter.')
-start_year = input().split(", ")
-print('Какие год окончания съёмки аниме вас интерсует? Если это для вас не важно нажмите Enter.')
-end_year = input().split(", ")
-print('Какие сезоны съёмки вас интересует? Если это для вас не важно нажмите Enter.')
-season_user = input().split(", ")
-print('Какая студия вас интересует? Если это для вас не важно нажмите Enter.')
-studios_user = input().split(", ")
+e = 'Если это для вас не важно нажмите Enter.'
+survey = [
+    'Какие жанры вас интересуют?',
+    'Какие предупреждения вас интересуют?',
+    'Какие типы вас интерсует?',
+    'Какие годы начала съёмки аниме вас интересует?',
+    'Какие годы окончания съёмки аниме вас интерсует?',
+    'Какие сезоны съёмки вас интересует?',
+    'Какая студия вас интересует?',
+    'Какой минимальный рейтинг вас интересует? '
+    'Введите число, если оно не целое то используйте точку для '
+    'разделения '
+    'целой и десятичной части.',
+    'Какое минимальное количество голосов вас интересует?',
+    'Какое минимальное количество эпизодов вас интересует?',
+    'Важно для вас чтобы аниме ещё продолжало выпускатьcя или '
+    'уже закончило? '
+    'Введите "True" или "False".'
+]
+
+answer = {
+    'tags_user': None,
+    'con_war': None,
+    'type_user': None,
+    'start_year': None,
+    'end_year': None,
+    'season_user': None,
+    'studios_user': None,
+    'min_rating': None,
+    'min_number_votes': None,
+    'min_episodes': None,
+    'finish_user': None
+}
+
+check = 0
+for i in answer:
+    print(survey[check] + e)
+    check += 1
+    if check <= 7:
+        answer[i] = input().split(", ")
+    if check > 7:
+        answer[i] = input()
+    if 7 < check < 11:
+        try:
+            answer[i] = float(answer[i])
+        except ValueError:
+            answer[i] = 0
+
+list_sort = [
+    ['Tags', sort_list, answer['tags_user']],
+    ['Content Warning', sort_list, answer['con_war']],
+    ['Type', sort_list, answer['type_user']],
+    ['StartYear', sort_list, answer['start_year']],
+    ['EndYear', sort_list, answer['end_year']],
+    ['Season', sort_list, answer['season_user']],
+    ['Studios', sort_list, answer['studios_user']],
+    ['Rating Score', sort_num_val, answer['min_rating']],
+    ['Number Votes', sort_num_val, answer['min_number_votes']],
+    ['Episodes', sort_num_val, answer['min_episodes']],
+    ['Finished', sort_list, answer['finish_user']]
+]
 
 with open('anime.csv', 'r', encoding='utf-8') as file:
     c = csv.DictReader(file, delimiter=',', quotechar='\"')
     arr = []
     for row in c:
-        if filter_('Rating Score', num_val, min_rating, row) and filter_('Number Votes', num_val, min_number_votes, row):
-            if filter_('Tags', genre, tags_user, row) and filter_('Content Warning', lists, con_war, row) and filter_('Type', lists, type_user, row):
-                if filter_('Episodes', num_val, min_episodes, row) and filter_('StartYear', lists, start_year, row) and filter_('EndYear', lists, end_year, row):
-                    if filter_('Season', lists, season_user, row) and filter_('Studios', lists, studios_user, row):
-                        if row['Rating Score'] == 'Unknown':
-                            arr.append([float(0), row['Name'], row['Url']])
-                        else:
-                            arr.append([float(row['Rating Score']), row['Name'], row['Url']])
+        result = 0
+        for i in range(len(list_sort)):
+            if sort_(list_sort[i][0], list_sort[i][1], list_sort[i][2],
+                     row):
+                result += 1
+            if result == 11:
+                if row['Rating Score'] == 'Unknown':
+                    arr.append([float(0), row['Name'], row['Url']])
+                else:
+                    arr.append([float(row['Rating Score']), row['Name'],
+                                row['Url']])
 arr.sort()
 arr.reverse()
 
@@ -99,14 +113,20 @@ pics = 0
 for i in range(0, len(arr)):
     response = requests.get(arr[i][2])
     soup = BeautifulSoup(response.text, 'html.parser')
-    img = requests.get("https://www.anime-planet.com/" + soup.find('img', class_ = 'screenshots')['src'])
-    img_file = open(str(i+1) + '.jpg', 'wb')
+    img = requests.get("https://www.anime-planet.com/"
+                       + soup.find('img', class_='screenshots')['src'])
+    img_file = open(str(i + 1) + '.jpg', 'wb')
     img_file.write(img.content)
     img_file.close()
     pics += 1
     if pics == 5:
         break
 
-with open('top.txt', 'w', encoding='utf-8') as file:
+with open('Top.txt', 'w', encoding='utf-8') as file:
     for i in range(len(arr)):
-        file.write(str(i+1)+ ' ' + arr[i][1] + arr[i][2] + '\n')
+        file.write(str(i + 1) + ' ' + arr[i][1] + arr[i][2] + '\n')
+
+print(
+    'Результат вашего поиска лежит в папке Top.txt, и отсортированно '
+    'по рейтингу. Плакаты к первым пяти аниме названы в порядке '
+    'убывания. Приятного просмотра)')
